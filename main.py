4eaf143450465f25ca3fc0318f7cb54743e3a0e7
Mohammad Mowlavi <79265093+MohammadMow@users.noreply.github.com>
2025-04-27 00:32:12 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
