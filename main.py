7af36b633b92c12ca1b401429874967badb1b572
Mohammad Mowlavi <79265093+MohammadMow@users.noreply.github.com>
2025-04-27 00:25:38 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    with open("exoloit.html", "r", encoding="utf-8") as file:
+        content = file.read()
+    return HTMLResponse(content=content)
