532bcebd43da7f62fb04ebdaee32ca60cb72a3a4
Mohammad Mowlavi <79265093+MohammadMow@users.noreply.github.com>
2025-04-27 00:53:46 +0330

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
-    with open("exoloit.html", "r", encoding="utf-8") as file:
+    with open("exploit.html", "r", encoding="utf-8") as file:
         content = file.read()
     return HTMLResponse(content=content)
